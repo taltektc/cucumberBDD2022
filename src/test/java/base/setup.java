a3a6f5572bcca3581e4666fff4201f52fd27727b
Hassan Bhuiyan/LGEUS Americas Information Center(hassan01.bhuiyan@lge.com) <hassan01.bhuiyan@lge.com>
2022-03-06 11:23:57 -0500

    headless and incognito window prec

--- src/test/java/base/setup.java
+++ src/test/java/base/setup.java
@@ -3,6 +3,7 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import java.time.Duration;
@@ -14,8 +15,11 @@ public class setup {
     // browser setup
     public static WebDriver setupBrowser(String driverType) {
         if (driverType.equalsIgnoreCase("ch")) {
+            ChromeOptions options = new ChromeOptions();
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            //options.setHeadless(true);
+            options.addArguments("--incognito");
+            driver = new ChromeDriver(options);
         } else if (driverType.equalsIgnoreCase("ff")) {
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
